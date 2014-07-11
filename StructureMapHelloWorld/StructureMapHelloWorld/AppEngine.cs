e8166573094d610d712afd45ec3210d9fddc4053
varunpathak <varunpthak123@gmail.com>
2014-07-11 17:36:30 +0530

    structure map with factory pattern
    start up project

---
+++ StructureMapHelloWorld/StructureMapHelloWorld/AppEngine.cs
@@ -0,0 +1,32 @@
+﻿
+namespace StructureMapHelloWorld
+{
+
+    public interface IFoodEngine
+    {
+        void Run();
+    }
+
+    public interface IFood
+    {
+        string GetFood();
+    }
+
+    public class SouthIndian : IFood
+    {
+        public string GetFood()
+        {
+            return "Idly-Dosa";
+        }
+    }
+
+    public class Italian : IFood
+    {
+        public string GetFood()
+        {
+            return "Pizza";
+        }
+    }
+
+
+}
---
+++ StructureMapHelloWorld/StructureMapHelloWorld/Program.cs
@@ -0,0 +1,72 @@
+﻿using StructureMap;
+using System;
+
+namespace StructureMapHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureDependencies();
+
+
+            Console.WriteLine("Type indian for Indian food / type italian for Italian Food / Press E to Exit");
+
+            string foodKind = Console.ReadLine();
+
+            while (String.IsNullOrEmpty(foodKind) == false && foodKind.ToLower() != "e")
+            {
+
+                IFood food;
+
+                if (foodKind.ToLower().Contains("italian"))
+                {
+                    food = ObjectFactory.GetNamedInstance<IFood>("ItalianFoodKey");
+                    Console.WriteLine("Thanks for Ordering " + food.GetFood());
+                    foodKind = Console.ReadLine();
+                }
+
+                else if (foodKind.ToLower().Contains("indian"))
+                {
+                    food = ObjectFactory.GetNamedInstance<IFood>("IndianFoodKey");
+                    Console.WriteLine("Thanks for Ordering " + food.GetFood());
+                    foodKind = Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("Type indian for Indian food / type italian for Italian Food / Press E to Exit");
+                    foodKind = Console.ReadLine();
+                }
+
+
+            }
+
+            Environment.Exit(0);
+
+        }
+
+
+        private static void ConfigureDependencies()
+        {
+            //return new Container(x =>
+            //{
+            //    x.For<IAppEngine>().Use<AppEngine>();
+            //    x.For<IGreeter>().Use<FrenchGreeter>();
+            //    x.For<IOutputDisplay>().Use<ConsoleOutputDisplay>();
+            //});
+
+            ObjectFactory.Initialize(x =>
+            {
+                // We put the properties for an NHibernate ISession
+                // in the StructureMap.config file, so this file
+                // must be there for our application to
+                // function correctly
+                //http://docs.structuremap.net/ConfiguringStructureMap.htm
+                x.UseDefaultStructureMapConfigFile = true;
+                //x.PullConfigurationFromAppConfig = true;
+            });
+
+        }
+    }
+}
